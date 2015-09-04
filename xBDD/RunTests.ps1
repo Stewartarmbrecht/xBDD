4300dfb96b9ccfccc4911e880db35a847eed4cf1
Stewart Armbrecht <stewart.armbrecht@protiviti.com>
2015-09-04 00:58:35 -0500

    Getting CI to build, test, and publish.

--- xBDD/RunTests.ps1
+++ xBDD/RunTests.ps1
@@ -1,3 +1,11 @@
+Param([string]$conn="")
+
+if($conn -ne "")
+{
+	$env:Data:DefaultConnection:ConnectionSrting=$conn
+	$env:Data:DefaultConnection:ConnectionSrting
+}
+
 dnu restore
 dnx $PSScriptRoot\test\xBDD.Test test
 .\xsltproc $PSScriptRoot\TestResults.xml $PSScriptRoot\NUnitXml.xslt $PSScriptRoot\NUnitTestResults.xml
