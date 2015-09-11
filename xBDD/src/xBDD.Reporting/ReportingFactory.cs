1dc7bd0ba9521dcff6c52ceba23dd429a05a4c7f
Stewart Armbrecht <stewart.armbrecht@protiviti.com>
2015-09-11 00:15:15 -0500

    Fixing build by adding a missing file.

---
+++ xBDD/src/xBDD.Reporting/ReportingFactory.cs
@@ -0,0 +1,12 @@
+﻿using xBDD.Reporting.TextFile;
+
+namespace xBDD.Reporting
+{
+    public class ReportingFactory : IReportingFactory
+    {
+        public ITextFileWriter GetTextFileWriter()
+        {
+            return new TextFileWriter();
+        }
+    }
+}
